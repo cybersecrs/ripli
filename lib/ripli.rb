2e16efa2224495402496c5323f833ce04eb9dcfd
Linuxander <71899979+cybersecrs@users.noreply.github.com>
2020-09-30 01:18:00 +0200

    Created Project for Hacktoberfet 2020
    Starting Ocober 3th

---
+++ lib/ripli.rb
@@ -0,0 +1,40 @@
+#================================================================================#
+#                      Ruby Proxychains List Downloader                          #
+#--------------------------------------------------------------------------------#
+#                                                                                #
+#   ************************                                                     #
+#   *                      *                                                     #
+#   *  [Ripli] Downloader  *  by Cybersec-RS                                     #
+#   *                      *                                                     #
+#   ************************                                                     #
+#                                                                                #
+#  ============================================================================  #
+#      Version: [0.1.0]   -   https://www.github.com/cybersecrs/ripli            #
+#  ============================================================================  #
+#                                                                                #
+#       Author: Linuxander                                                       #
+#                                                                                #
+#  Description: Project for HACKTOBERFEST-2020                                   #
+#               Scrap the internet for free elite proxy servers                  #
+#                                                                                #
+#               + Save them in Proxychains type (socks5  ip  port)               #   
+#               + Each class should scrap one web site                           #
+#               + Maximum timeout is 1000ms                                      #
+#               + Should add tests for country, data leak, privacy               #
+#                                                                                #
+#         Note: Please follow Hacktoberfest rules about pull requests!           #
+#                                                                                #
+#      ====================================================================      #
+#                    HAPPY HACKING & MAKE IT OPEN SOURCE !!!                     #
+#      ====================================================================      #
+#                                                                                #
+##################################################################################
+
+require_relative "ripli/version"
+require_relative "ripli/proxyscrape"
+
+module Ripli
+
+  class Error < StandardError; end
+
+end
