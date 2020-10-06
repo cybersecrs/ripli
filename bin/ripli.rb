fd58c8716b634d3f8c1e9e5090eb26c79cb0bd0a
linuxander <linuxander@secmail.pro>
2020-10-07 00:33:12 +0200

    mistake fixed

--- bin/ripli.rb
+++ bin/ripli.rb
@@ -3,11 +3,6 @@
 require_relative '../lib/ripli.rb'
 require 'pry'
 
-<<<<<<< HEAD
-Ripli::ProxyScrape.new.shell_exec!
-Ripli::HideMyName.new.shell_exec!
-Ripli::ProxyScan.new.shell_exec!
-=======
 AVAILABLE_TYPES = %w[https socks4 socks5].freeze
 
 opts = Optimist.options do
@@ -19,4 +14,4 @@ if (opts.type.uniq - AVAILABLE_TYPES).any?
 end
 
 Ripli::CustomParser.descendants.each { |custom_parser| custom_parser.new.shell_exec!(opts.type) }
->>>>>>> 44098c595dea793bb8e23ee4db3184e13737bb6e
+
